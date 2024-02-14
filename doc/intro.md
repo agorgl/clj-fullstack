e8135c57ea4f8c07f13e13f05d7e432fc424b610
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-15 00:34:17 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to agorgl/clj-fullstack
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/agorgl/clj_fullstack_test.clj
@@ -0,0 +1,13 @@
+(ns agorgl.clj-fullstack-test
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.test :refer :all]
+            [org.corfield.new] ; for the Specs
+            [agorgl.clj-fullstack :refer :all]))
+
+(deftest valid-template-test
+  (testing "template.edn is valid."
+    (let [template (edn/read-string (slurp (io/resource "agorgl/clj_fullstack/template.edn")))]
+      (is (s/valid? :org.corfield.new/template template)
+          (s/explain-str :org.corfield.new/template template)))))
