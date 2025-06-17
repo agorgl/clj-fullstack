d6cf97a67bc2e829d3700ff7fd864528934bdf2d
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-06-17 19:34:05 +0300

    cljs: ensure target/classes is on classpath before starting server

--- dev/clj/repl.clj
+++ dev/clj/repl.clj
@@ -1,8 +1,28 @@
 (ns repl
   (:require
+   [clojure.java.io :as io]
    [shadow.cljs.devtools.server :as shadow-server]
    [task]))
 
+(defn ensure-classpath-entry [path]
+  (try
+    (.mkdirs (io/file path))
+    (let [loader (-> (Thread/currentThread)
+                     (.getContextClassLoader))
+          method (->> loader
+                      (.getClass)
+                      (.getSuperclass)
+                      (.getDeclaredMethods)
+                      (filter #(= (.getName %) "appendClassPath"))
+                      (first))]
+      (.setAccessible method true)
+      (.invoke method loader (into-array Object [path])))
+    (catch Exception e
+      (binding [*out* *err*]
+        (println (format "Could not add entry \"%s\" to classpath" path)))
+      (.printStackTrace e))))
+
 (defn -main [& args]
+  (ensure-classpath-entry "target/classes")
   (shadow-server/start!)
   (task/watches-start))
