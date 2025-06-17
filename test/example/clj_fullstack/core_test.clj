9ca17d35798e2803abc673b02eed34e8cfb5ef9a
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-06-17 16:43:57 +0300

    project: beautify generated sources

--- test/example/clj_fullstack/core_test.clj
+++ test/example/clj_fullstack/core_test.clj
@@ -1,6 +1,7 @@
 (ns example.clj-fullstack.core-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [example.clj-fullstack.core :as sut])) ; system under test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [example.clj-fullstack.core :as sut])) ; system under test
 
 (deftest a-test
   (testing "FIXME, I fail."
