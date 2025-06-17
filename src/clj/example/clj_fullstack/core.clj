7dd67c7f57ab995f2fad5d4caf221941cb58047a
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-06-17 18:34:51 +0300

    core: add server lifecycle on main entrypoint

--- src/clj/example/clj_fullstack/core.clj
+++ src/clj/example/clj_fullstack/core.clj
@@ -1,7 +1,19 @@
 (ns example.clj-fullstack.core
   (:require
-   [clojure.tools.logging :as log])
+   [clojure.tools.logging :as log]
+   [example.clj-fullstack.server :as server])
   (:gen-class))
 
+(defn add-shutdown-hook [f]
+  (let [shutdown-fn
+        (fn []
+          (f)
+          (shutdown-agents))]
+    (-> (Runtime/getRuntime)
+        (.addShutdownHook
+         (Thread. shutdown-fn)))))
+
 (defn -main [& args]
-  (log/info "Hello there"))
+  (log/info "Hello there")
+  (let [server (server/start)]
+    (add-shutdown-hook #(server/stop server))))
